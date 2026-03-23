9c57af4561017e63edfe2494cf45e2bf00ddbfac
hanrrysantos <hanrry.jsantos@gmailcom>
2026-03-23 09:33:55 -0300

    criacao do projeto - inventory manager

---
+++ src/test/java/br/com/hanrry/inventory/InventoryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hanrry.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
