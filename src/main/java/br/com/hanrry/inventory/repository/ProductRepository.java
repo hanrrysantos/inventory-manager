58efef6a75d851e5e531d49cd07735b3fe1c56a2
hanrrysantos <hanrry.jsantos@gmailcom>
2026-03-23 12:07:13 -0300

    criando layer repository de cada entidade

---
+++ src/main/java/br/com/hanrry/inventory/repository/ProductRepository.java
@@ -0,0 +1,16 @@
+package br.com.hanrry.inventory.repository;
+
+import br.com.hanrry.inventory.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Optional<Product> findBySku(String sku);
+
+    List<Product> findByCategoryId(Long categoryId);
+
+    List<Product> findByNameContainingIgnoreCase(String name);
+}
