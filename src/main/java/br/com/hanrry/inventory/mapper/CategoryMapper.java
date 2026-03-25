f6e92ea70fd2e0a805c0949ec8b86f1d0a688412
hanrrysantos <hanrry.jsantos@gmailcom>
2026-03-25 09:37:10 -0300

    criacao do Mapper da classe category

--- src/main/java/br/com/hanrry/inventory/mapper/CategoryMapper.java
+++ src/main/java/br/com/hanrry/inventory/mapper/CategoryMapper.java
@@ -1,9 +1,20 @@
 package br.com.hanrry.inventory.mapper;
 
+import br.com.hanrry.inventory.dto.category.CategoryRequestDTO;
+import br.com.hanrry.inventory.dto.category.CategoryResponseDTO;
+import br.com.hanrry.inventory.entity.Category;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
-public class CategoryMapper {
+public interface CategoryMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "products", ignore = true)
+    Category toEntity(CategoryRequestDTO request);
+
+    CategoryResponseDTO toDTO(Category category);
 
 }
