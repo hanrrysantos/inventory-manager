974dd32aeae2cea0c330dbc05809a4bbd1f8c291
hanrrysantos <hanrry.jsantos@gmailcom>
2026-03-25 09:17:30 -0300

    criando entidades user e suas roles

---
+++ src/main/java/br/com/hanrry/inventory/entity/User.java
@@ -0,0 +1,39 @@
+package br.com.hanrry.inventory.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Min(8)
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+}
