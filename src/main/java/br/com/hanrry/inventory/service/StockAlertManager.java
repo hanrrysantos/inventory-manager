5ed670a1432bab6bc7ce0a2894ef50f5c8a84e4b
hanrrysantos <hanrry.jsantos@gmail.com>
2026-03-26 11:01:03 -0300

    logica do alerta de produtos que estao com estoque abaixo do minimo definido

---
+++ src/main/java/br/com/hanrry/inventory/service/StockAlertManager.java
@@ -0,0 +1,48 @@
+package br.com.hanrry.inventory.service;
+
+import br.com.hanrry.inventory.dto.product.ProductResponseDTO;
+import br.com.hanrry.inventory.entity.Batch;
+import br.com.hanrry.inventory.entity.Product;
+import br.com.hanrry.inventory.exception.product.ProductNotFoundException;
+import br.com.hanrry.inventory.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class StockAlertManager {
+
+    private final ProductRepository productRepository;
+    private final ProductService productService;
+    private final PdfService pdfService;
+    private final EmailService emailService;
+
+    public void checkLowStockAndNotify(Long productId) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new ProductNotFoundException("Product not found with id: " + productId));
+
+        long currentStock = product.getBatches().stream()
+                .mapToLong(Batch::getQuantity)
+                .sum();
+
+        if (currentStock <= product.getMinStock()) {
+            this.sendNotificationWithReport(product.getName());
+        }
+    }
+
+    private void sendNotificationWithReport(String productName) {
+        byte[] pdfReport = generateLowStockReport();
+        if (pdfReport.length > 0) {
+            emailService.sendLowStockAlert(productName, pdfReport);
+        }
+    }
+
+    private byte[] generateLowStockReport() {
+        List<ProductResponseDTO> lowStockProducts = productService.getLowStockProducts();
+        if (lowStockProducts.isEmpty()) return new byte[0];
+
+        return pdfService.generateLowStockReport(lowStockProducts);
+    }
+}
