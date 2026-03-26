1306df0a51f9a33c9707c70de5f0cd22bfe5ec68
hanrrysantos <hanrry.jsantos@gmail.com>
2026-03-26 10:14:36 -0300

    productController finalizado

--- src/main/java/br/com/hanrry/inventory/controller/ProductController.java
+++ src/main/java/br/com/hanrry/inventory/controller/ProductController.java
@@ -5,9 +5,6 @@ import br.com.hanrry.inventory.dto.product.ProductResponseDTO;
 import br.com.hanrry.inventory.dto.product.UpdateProdcutRequestDTO;
 import br.com.hanrry.inventory.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -16,8 +13,8 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/products")
 public class ProductController {
 
     private final ProductService productService;
@@ -42,11 +39,8 @@ public class ProductController {
     }
 
     @GetMapping()
-    public ResponseEntity<Page<ProductResponseDTO>> findAllProducts(
-            @PageableDefault(size = 3, sort = "id")
-            Pageable pageable
-    ){
-        Page<ProductResponseDTO> productList = productService.findAllProducts(pageable);
+    public ResponseEntity<List<ProductResponseDTO>> findAllProducts(){
+        List<ProductResponseDTO> productList = productService.findAllProducts();
 
         return ResponseEntity.ok().body(productList);
     }
