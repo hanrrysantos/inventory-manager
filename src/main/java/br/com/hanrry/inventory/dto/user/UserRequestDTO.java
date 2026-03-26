4bd0a94cc57285ef38ddf85a9ee296f0186e0127
hanrrysantos <hanrry.jsantos@gmail.com>
2026-03-26 10:15:22 -0300

    corrigindo dto de saida e user

--- src/main/java/br/com/hanrry/inventory/dto/user/UserRequestDTO.java
+++ src/main/java/br/com/hanrry/inventory/dto/user/UserRequestDTO.java
@@ -1,7 +1,17 @@
 package br.com.hanrry.inventory.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record UserRequestDTO (
+
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email format")
         String email,
+
+        @NotBlank(message = "Password is required")
+        @Size(min = 6, message = "Password must have at least 6 characters")
         String password
 ){
 }
