9b8b5829abf338d6b6314465b72c3f20fcad72a4
hanrrysantos <hanrry.jsantos@gmail.com>
2026-03-26 10:17:56 -0300

    productservice finalizado

--- src/main/java/br/com/hanrry/inventory/service/ProductService.java
+++ src/main/java/br/com/hanrry/inventory/service/ProductService.java
@@ -3,44 +3,52 @@ package br.com.hanrry.inventory.service;
 import br.com.hanrry.inventory.dto.product.ProductRequestDTO;
 import br.com.hanrry.inventory.dto.product.ProductResponseDTO;
 import br.com.hanrry.inventory.dto.product.UpdateProdcutRequestDTO;
+import br.com.hanrry.inventory.entity.Category;
 import br.com.hanrry.inventory.entity.Product;
+import br.com.hanrry.inventory.exception.category.CategoryNotFoundException;
 import br.com.hanrry.inventory.exception.product.ProductAlreadyExistsException;
 import br.com.hanrry.inventory.exception.product.ProductNotFoundException;
 import br.com.hanrry.inventory.mapper.ProductMapper;
+import br.com.hanrry.inventory.repository.CategoryRepository;
 import br.com.hanrry.inventory.repository.ProductRepository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class ProductService {
 
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
-    public ProductService(ProductRepository productRepository, ProductMapper productMapper){
-        this.productRepository = productRepository;
-        this.productMapper = productMapper;
-    }
+    private final CategoryRepository categoryRepository;
 
+    @Transactional
     public ProductResponseDTO createProduct(ProductRequestDTO request){
 
         productRepository.findBySku(request.sku()).ifPresent(
                p -> {
                    throw new ProductAlreadyExistsException("Product already exists");
                });
+        Category category = categoryRepository.findById(request.categoryId())
+                .orElseThrow(() -> new CategoryNotFoundException("Category not found"));
+
        Product product = productMapper.toEntity(request);
+
+       product.setCategory(category);
 
        Product savedProduct = productRepository.save(product);
 
        return productMapper.toDTO(savedProduct);
     }
 
-    public Page<ProductResponseDTO> findAllProducts(Pageable pageable){
-        Page<Product> productPage = productRepository.findAll(pageable);
+    public List<ProductResponseDTO> findAllProducts( ){
+        List<Product> productsList = productRepository.findAll();
 
-        return productPage.map(productMapper::toDTO);
+        return productMapper.toDTOList(productsList);
     }
 
     public ProductResponseDTO findProductById(Long id){
@@ -51,6 +59,7 @@ public class ProductService {
         return productMapper.toDTO(product);
     }
 
+    @Transactional
     public ProductResponseDTO updateProduct(Long id, UpdateProdcutRequestDTO request){
         Product product = productRepository.findById(id).orElseThrow(
                 () -> new ProductNotFoundException("Product not found with this id: " + id)
@@ -71,6 +80,7 @@ public class ProductService {
         return productMapper.toDTO(savedProduct);
     }
 
+    @Transactional
     public void deleteProductById(Long id){
         findProductById(id);
         productRepository.deleteById(id);
