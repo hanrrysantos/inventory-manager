c7765e6a63005b02184f139ee8d65cc0c1a0f63c
hanrrysantos <hanrry.jsantos@gmail.com>
2026-03-26 10:16:58 -0300

    batchController finalizada

--- src/main/java/br/com/hanrry/inventory/controller/BatchController.java
+++ src/main/java/br/com/hanrry/inventory/controller/BatchController.java
@@ -2,39 +2,55 @@ package br.com.hanrry.inventory.controller;
 
 import br.com.hanrry.inventory.dto.batch.BatchRequestDTO;
 import br.com.hanrry.inventory.dto.batch.BatchResponseDTO;
+import br.com.hanrry.inventory.dto.batch.ConsumeBatchRequestDTO;
 import br.com.hanrry.inventory.service.BatchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/batches")
+@RequestMapping("/api/v1/batches")
 @RequiredArgsConstructor
 public class BatchController {
 
     private final BatchService batchService;
 
     @PostMapping
-    public ResponseEntity<BatchResponseDTO> createBatch(@RequestBody BatchRequestDTO request) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(batchService.createBatch(request));
+    public ResponseEntity<BatchResponseDTO> createBatch(
+            @RequestBody BatchRequestDTO request
+    ){
+        BatchResponseDTO batch = batchService.createBatch(request);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(batch.id()).toUri();
+        return ResponseEntity.created(uri).body(batch);
     }
 
     @PatchMapping("/{id}/add")
-    public ResponseEntity<BatchResponseDTO> addStock(@PathVariable Long id, @RequestParam Long quantity) {
-        return ResponseEntity.ok(batchService.addStockToBatch(id, quantity));
+    public ResponseEntity<BatchResponseDTO> addStock(
+            @PathVariable Long id,
+            @RequestBody BatchRequestDTO request
+    ){
+        BatchResponseDTO batch = batchService.addStock(id, request);
+        return ResponseEntity.ok().body(batch);
     }
 
     @PostMapping("/consume")
-    public ResponseEntity<Void> consume(@RequestParam Long productId, @RequestParam Long quantity) {
-        batchService.consumeStock(productId, quantity);
+    public ResponseEntity<Void> consume(
+            @RequestBody ConsumeBatchRequestDTO request
+    ){
+        batchService.consumeStock(request);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/expired")
-    public ResponseEntity<List<BatchResponseDTO>> listExpired() {
-        return ResponseEntity.ok(batchService.findExpiredBatches());
+    public ResponseEntity<List<BatchResponseDTO>> listExpired(
+    ){
+        List<BatchResponseDTO> batchesList = batchService.findExpiredBatches();
+        return ResponseEntity.ok().body(batchesList);
     }
 }
