8bf5be79ccccb75b96e9bd839ad8972dd9bc28ed
hanrrysantos <hanrry.jsantos@gmail.com>
2026-03-29 20:50:51 -0300

    logica do login com jwt

--- src/main/java/br/com/hanrry/inventory/security/JwtAuthenticationFilter.java
+++ src/main/java/br/com/hanrry/inventory/security/JwtAuthenticationFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,16 +15,12 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
 
     private final UserDetailsServiceImpl userDetailsServiceImpl;
-
-    public JwtAuthenticationFilter(JwtUtil jwtUtil, UserDetailsServiceImpl userDetailsServiceImpl){
-        this.jwtUtil = jwtUtil;
-        this.userDetailsServiceImpl = userDetailsServiceImpl;
-    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
