fbb171885229d94c75c3954df88291ad6d4e4c06
hanrrysantos <hanrry.jsantos@gmail.com>
2026-03-29 20:49:19 -0300

    adicionando mapper para toDTOList

--- src/main/java/br/com/hanrry/inventory/mapper/UserMapper.java
+++ src/main/java/br/com/hanrry/inventory/mapper/UserMapper.java
@@ -5,11 +5,15 @@ import br.com.hanrry.inventory.dto.user.UserResponseDTO;
 import br.com.hanrry.inventory.entity.User;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
     UserResponseDTO toDTO(User user);
 
     User toEntity(UserRequestDTO request);
+
+    List<UserResponseDTO> toDTOList(List<User> userList);
 
 }
