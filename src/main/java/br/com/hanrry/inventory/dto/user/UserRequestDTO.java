6bb9ae0489804960b3ba16e28e9c94c13fa13528
hanrrysantos <hanrry.jsantos@gmail.com>
2026-03-31 10:24:00 -0300

    alterando exemplo de email e senha no swagger

--- src/main/java/br/com/hanrry/inventory/dto/user/UserRequestDTO.java
+++ src/main/java/br/com/hanrry/inventory/dto/user/UserRequestDTO.java
@@ -12,12 +12,12 @@ public record UserRequestDTO (
 
         @NotBlank(message = "Email is required")
         @Email(message = "Invalid email format")
-        @Schema(example = "admin@email.com")
+        @Schema(example = "user1@email.com")
         String email,
 
         @NotBlank(message = "Password is required")
         @Size(min = 6, message = "Password must have at least 6 characters")
-        @Schema(example = "admin123")
+        @Schema(example = "user1234")
         String password
 ){
 }
