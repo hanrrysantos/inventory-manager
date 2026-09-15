2e2a5cb67515b9f7ab927325f34aab601c4cdd71
Hanrry <hanrry.jsantos@gmail.com>
2026-09-15 11:56:53 -0300

    organizando dominio de usuario

---
+++ src/main/java/br/com/hanrry/inventory/user/dto/UserRequestDTO.java
@@ -0,0 +1,24 @@
+package br.com.hanrry.inventory.user.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserRequestDTO (
+
+        @Schema(example = "user1")
+        @NotBlank
+        String name,
+
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email format")
+        @Schema(example = "user1@email.com")
+        String email,
+
+        @NotBlank(message = "Password is required")
+        @Size(min = 6, message = "Password must have at least 6 characters")
+        @Schema(example = "user1234")
+        String password
+){
+}
