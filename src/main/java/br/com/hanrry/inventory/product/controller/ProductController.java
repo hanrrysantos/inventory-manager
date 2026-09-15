84121c9e7f9b7a476902457d6616eacb0d0d1780
Hanrry <hanrry.jsantos@gmail.com>
2026-09-15 11:56:53 -0300

    organizando dominio de produto

---
+++ src/main/java/br/com/hanrry/inventory/product/controller/ProductController.java
@@ -0,0 +1,72 @@
+package br.com.hanrry.inventory.product.controller;
+
+import br.com.hanrry.inventory.product.controller.docs.ProductControllerDocs;
+import br.com.hanrry.inventory.product.dto.product.ProductRequestDTO;
+import br.com.hanrry.inventory.product.dto.product.ProductResponseDTO;
+import br.com.hanrry.inventory.product.dto.product.UpdateProdcutRequestDTO;
+import br.com.hanrry.inventory.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/products")
+public class ProductController implements ProductControllerDocs {
+
+    private final ProductService productService;
+
+    @GetMapping()
+    public ResponseEntity<List<ProductResponseDTO>> findAllProducts(){
+        List<ProductResponseDTO> productList = productService.findAllProducts();
+
+        return ResponseEntity.ok().body(productList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponseDTO> findProductById(
+            @PathVariable Long id
+    ){
+        ProductResponseDTO product = productService.findProductById(id);
+
+        return ResponseEntity.ok().body(product);
+    }
+
+    @GetMapping("/low-stock")
+    public ResponseEntity<List<ProductResponseDTO>> getLowStock(){
+        List<ProductResponseDTO> productList = productService.getLowStockProducts();
+
+        return ResponseEntity.ok().body(productList);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductResponseDTO> createProduct(
+            @RequestBody ProductRequestDTO request
+    ) {
+        ProductResponseDTO product = productService.createProduct(request);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(product.id()).toUri();
+        return ResponseEntity.created(uri).body(product);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponseDTO> updateProduct(
+            @PathVariable Long id,
+            @RequestBody UpdateProdcutRequestDTO request
+    ){
+        ProductResponseDTO product = productService.updateProduct(id, request);
+        return ResponseEntity.ok().body(product);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteProduct(
+            @PathVariable Long id
+    ){
+        productService.deleteProductById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
