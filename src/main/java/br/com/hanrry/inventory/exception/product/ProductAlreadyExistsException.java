cc4afc6a0b3649c25977568ee3bfbc2582e8205f
Hanrry <hanrry.jsantos@gmail.com>
2026-09-15 11:56:53 -0300

    organizando dominio de produto

--- src/main/java/br/com/hanrry/inventory/exception/product/ProductAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package br.com.hanrry.inventory.exception.product;
-
-public class ProductAlreadyExistsException extends RuntimeException {
-    public ProductAlreadyExistsException(String message) {
-        super(message);
-    }
-}
