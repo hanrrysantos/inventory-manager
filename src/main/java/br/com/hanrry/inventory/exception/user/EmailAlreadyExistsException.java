cab9c4bec9b3b546d19c8adfe810e3d1974e6b61
Hanrry <hanrry.jsantos@gmail.com>
2026-09-15 11:56:53 -0300

    organizando dominio de usuario

--- src/main/java/br/com/hanrry/inventory/exception/user/EmailAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package br.com.hanrry.inventory.exception.user;
-
-public class EmailAlreadyExistsException extends RuntimeException {
-    public EmailAlreadyExistsException(String message) {
-        super(message);
-    }
-}
