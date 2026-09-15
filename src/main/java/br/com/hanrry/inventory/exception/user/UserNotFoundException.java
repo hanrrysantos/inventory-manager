04b05b423d3e6b2a5ccd6f038b41deb3a74c4cc6
Hanrry <hanrry.jsantos@gmail.com>
2026-09-15 11:56:53 -0300

    organizando dominio de usuario

--- src/main/java/br/com/hanrry/inventory/exception/user/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.com.hanrry.inventory.exception.user;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
