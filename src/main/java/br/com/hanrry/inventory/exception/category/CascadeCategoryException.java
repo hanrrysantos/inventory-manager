5a232f5fcd17ebe27a95fbcc259c823b51453f37
Hanrry <hanrry.jsantos@gmail.com>
2026-09-15 11:56:53 -0300

    organizando dominio de produto

--- src/main/java/br/com/hanrry/inventory/exception/category/CascadeCategoryException.java
+++
@@ -1,7 +0,0 @@
-package br.com.hanrry.inventory.exception.category;
-
-public class CascadeCategoryException extends RuntimeException {
-    public CascadeCategoryException(String message) {
-        super(message);
-    }
-}
