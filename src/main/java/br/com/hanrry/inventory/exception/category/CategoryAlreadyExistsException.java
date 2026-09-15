c57fe2f50c409210b2c352be854e826666d90bdc
Hanrry <hanrry.jsantos@gmail.com>
2026-09-15 11:56:53 -0300

    organizando dominio de produto

--- src/main/java/br/com/hanrry/inventory/exception/category/CategoryAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package br.com.hanrry.inventory.exception.category;
-
-public class CategoryAlreadyExistsException extends RuntimeException {
-    public CategoryAlreadyExistsException(String message) {
-        super(message);
-    }
-}
