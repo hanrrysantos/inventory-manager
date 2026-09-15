1867d94bfe05b794cf1848637f6fd9fbd9d672f2
Hanrry <hanrry.jsantos@gmail.com>
2026-09-15 11:56:53 -0300

    organizando dominio de produto

---
+++ src/main/java/br/com/hanrry/inventory/product/mapper/CategoryMapper.java
@@ -0,0 +1,22 @@
+package br.com.hanrry.inventory.product.mapper;
+
+import br.com.hanrry.inventory.product.dto.category.CategoryRequestDTO;
+import br.com.hanrry.inventory.product.dto.category.CategoryResponseDTO;
+import br.com.hanrry.inventory.product.entity.Category;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "products", ignore = true)
+    Category toEntity(CategoryRequestDTO request);
+
+    CategoryResponseDTO toDTO(Category category);
+
+    List<CategoryResponseDTO> toDTOList(List<Category> categories);
+
+}
