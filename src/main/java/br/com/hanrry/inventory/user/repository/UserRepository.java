bd5deb457322fcb14cb2b10d4a154b2c65acb50d
Hanrry <hanrry.jsantos@gmail.com>
2026-09-15 11:56:53 -0300

    organizando dominio de usuario

---
+++ src/main/java/br/com/hanrry/inventory/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package br.com.hanrry.inventory.user.repository;
+
+import br.com.hanrry.inventory.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
