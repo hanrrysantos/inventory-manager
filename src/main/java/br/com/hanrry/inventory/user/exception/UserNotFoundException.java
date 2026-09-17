69fd874863ecf1e62c54b4b55116ed93806d163e
Hanrry <hanrry.jsantos@gmail.com>
2026-09-17 14:32:08 -0300

    atualizando arquivo src/main/java/br/com/hanrry/inventory/user/exception/UserNotFoundException.java

--- src/main/java/br/com/hanrry/inventory/user/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.com.hanrry.inventory.user.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
