039533cc64c722c14e94156a7b78613702724b42
Hanrry <hanrry.jsantos@gmail.com>
2026-09-17 14:32:07 -0300

    atualizando arquivo src/main/java/br/com/hanrry/inventory/inventory/exception/batch/BatchAlreadyExists.java

--- src/main/java/br/com/hanrry/inventory/inventory/exception/batch/BatchAlreadyExists.java
+++
@@ -1,7 +0,0 @@
-package br.com.hanrry.inventory.inventory.exception.batch;
-
-public class BatchAlreadyExists extends RuntimeException {
-    public BatchAlreadyExists(String message) {
-        super(message);
-    }
-}
