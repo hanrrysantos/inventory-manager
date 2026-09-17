0b82bc413a4b3ed4c3aa0cb869254c92d800c959
Hanrry <hanrry.jsantos@gmail.com>
2026-09-17 14:32:08 -0300

    atualizando arquivo src/main/java/br/com/hanrry/inventory/shared/exception/notification/pdf/WritePdfException.java

---
+++ src/main/java/br/com/hanrry/inventory/shared/exception/notification/pdf/WritePdfException.java
@@ -0,0 +1,7 @@
+package br.com.hanrry.inventory.shared.exception.notification.pdf;
+
+public class WritePdfException extends RuntimeException {
+    public WritePdfException(String message) {
+        super(message);
+    }
+}
