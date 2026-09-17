189a451ee2e3b6d3399013cf0ea7e919435827ff
Hanrry <hanrry.jsantos@gmail.com>
2026-09-17 14:32:08 -0300

    atualizando arquivo src/main/java/br/com/hanrry/inventory/product/exception/product/ProductNotFoundException.java

--- src/main/java/br/com/hanrry/inventory/product/exception/product/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.com.hanrry.inventory.product.exception.product;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
