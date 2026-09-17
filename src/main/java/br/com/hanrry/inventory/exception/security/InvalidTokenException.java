c9a3e24b9d65782eca260a044aee37dba5cf8417
Hanrry <hanrry.jsantos@gmail.com>
2026-09-17 14:32:07 -0300

    atualizando arquivo src/main/java/br/com/hanrry/inventory/exception/security/InvalidTokenException.java

--- src/main/java/br/com/hanrry/inventory/exception/security/InvalidTokenException.java
+++
@@ -1,7 +0,0 @@
-package br.com.hanrry.inventory.exception.security;
-
-public class InvalidTokenException extends RuntimeException {
-    public InvalidTokenException(String message) {
-        super(message);
-    }
-}
