0be7933883cad780eb8503d61ad3c61cbc23073f
Hanrry <hanrry.jsantos@gmail.com>
2026-09-17 14:32:08 -0300

    atualizando arquivo src/test/java/br/com/hanrry/inventory/notification/serviceTest/StockAlertServiceTest.java

---
+++ src/test/java/br/com/hanrry/inventory/notification/serviceTest/StockAlertServiceTest.java
@@ -0,0 +1,79 @@
+package br.com.hanrry.inventory.notification.serviceTest;
+
+import br.com.hanrry.inventory.product.dto.product.ProductResponseDTO;
+import br.com.hanrry.inventory.notification.email.EmailService;
+import br.com.hanrry.inventory.notification.document.PdfService;
+import br.com.hanrry.inventory.product.service.ProductService;
+import br.com.hanrry.inventory.notification.service.StockAlertService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class StockAlertServiceTest {
+
+    @Mock
+    private ProductService productService;
+
+    @Mock
+    private PdfService pdfService;
+
+    @Mock
+    private EmailService emailService;
+
+    @InjectMocks
+    private StockAlertService stockAlertService;
+
+    @Test
+    void shouldSendLowStockAlertSuccessfully() {
+
+        ProductResponseDTO product =
+                new ProductResponseDTO(
+                        1L,
+                        "Notebook",
+                        "NOTE-001",
+                        5L,
+                        "Eletrônicos",
+                        10L
+                );
+
+        List<ProductResponseDTO> lowStockProducts = List.of(product);
+
+        byte[] pdfReport = new byte[]{1, 2, 3};
+
+        when(productService.getLowStockProducts())
+                .thenReturn(lowStockProducts);
+
+        when(pdfService.generateLowStockReport(lowStockProducts))
+                .thenReturn(pdfReport);
+
+        stockAlertService.checkInventoryAndNotify();
+
+        verify(productService).getLowStockProducts();
+
+        verify(pdfService).generateLowStockReport(lowStockProducts);
+
+        verify(emailService).sendLowStockAlert(
+                List.of("Notebook"),
+                pdfReport
+        );
+    }
+
+    @Test
+    void shouldNotSendEmailWhenNoLowStockProducts() {
+        when(productService.getLowStockProducts())
+                .thenReturn(List.of());
+
+        stockAlertService.checkInventoryAndNotify();
+
+        verify(productService).getLowStockProducts();
+        verifyNoInteractions(pdfService);
+        verifyNoInteractions(emailService);
+    }
+}
