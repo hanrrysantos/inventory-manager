22f4e9a0dcb17a601e55091a63fb0c0c3151b12b
Hanrry <hanrry.jsantos@gmail.com>
2026-09-17 14:32:08 -0300

    atualizando arquivo src/main/java/br/com/hanrry/inventory/shared/exception/product/category/CascadeCategoryException.java

---
+++ src/main/java/br/com/hanrry/inventory/shared/exception/product/category/CascadeCategoryException.java
@@ -0,0 +1,7 @@
+package br.com.hanrry.inventory.shared.exception.product.category;
+
+public class CascadeCategoryException extends RuntimeException {
+    public CascadeCategoryException(String message) {
+        super(message);
+    }
+}
