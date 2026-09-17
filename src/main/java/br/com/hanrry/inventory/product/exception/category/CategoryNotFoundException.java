84b34333fbb0f8f82094a2bf14a65e4e7aeac56c
Hanrry <hanrry.jsantos@gmail.com>
2026-09-17 14:32:08 -0300

    atualizando arquivo src/main/java/br/com/hanrry/inventory/product/exception/category/CategoryNotFoundException.java

--- src/main/java/br/com/hanrry/inventory/product/exception/category/CategoryNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.com.hanrry.inventory.product.exception.category;
-
-public class CategoryNotFoundException extends RuntimeException {
-    public CategoryNotFoundException(String message) {
-        super(message);
-    }
-}
