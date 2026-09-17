76d352e82249441f7c4fe3c66bd1260926111ebd
Hanrry <hanrry.jsantos@gmail.com>
2026-09-17 14:32:07 -0300

    atualizando arquivo src/main/java/br/com/hanrry/inventory/inventory/exception/batch/InvalidQuantityException.java

--- src/main/java/br/com/hanrry/inventory/inventory/exception/batch/InvalidQuantityException.java
+++
@@ -1,7 +0,0 @@
-package br.com.hanrry.inventory.inventory.exception.batch;
-
-public class InvalidQuantityException extends RuntimeException {
-    public InvalidQuantityException(String message) {
-        super(message);
-    }
-}
