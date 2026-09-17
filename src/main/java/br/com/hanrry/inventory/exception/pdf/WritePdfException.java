d50e3adcc4606895c9674e7dd232dca0d5993cab
Hanrry <hanrry.jsantos@gmail.com>
2026-09-17 14:32:07 -0300

    atualizando arquivo src/main/java/br/com/hanrry/inventory/exception/pdf/WritePdfException.java

--- src/main/java/br/com/hanrry/inventory/exception/pdf/WritePdfException.java
+++
@@ -1,7 +0,0 @@
-package br.com.hanrry.inventory.exception.pdf;
-
-public class WritePdfException extends RuntimeException {
-    public WritePdfException(String message) {
-        super(message);
-    }
-}
