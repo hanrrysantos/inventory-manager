686994e591b5f9952b9214c4ed5d43575260d227
Hanrry <hanrry.jsantos@gmail.com>
2026-09-17 14:32:07 -0300

    atualizando arquivo src/main/java/br/com/hanrry/inventory/inventory/exception/batch/InsufficientStockException.java

--- src/main/java/br/com/hanrry/inventory/inventory/exception/batch/InsufficientStockException.java
+++
@@ -1,7 +0,0 @@
-package br.com.hanrry.inventory.inventory.exception.batch;
-
-public class InsufficientStockException extends RuntimeException {
-    public InsufficientStockException(String message) {
-        super(message);
-    }
-}
