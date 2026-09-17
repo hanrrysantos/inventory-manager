9e332bc094568522db7b1204eacf5d4f4285f6fb
Hanrry <hanrry.jsantos@gmail.com>
2026-09-17 14:32:08 -0300

    atualizando arquivo src/main/java/br/com/hanrry/inventory/product/exception/category/CategoryAlreadyExistsException.java

--- src/main/java/br/com/hanrry/inventory/product/exception/category/CategoryAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package br.com.hanrry.inventory.product.exception.category;
-
-public class CategoryAlreadyExistsException extends RuntimeException {
-    public CategoryAlreadyExistsException(String message) {
-        super(message);
-    }
-}
